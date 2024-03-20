8597228cf739653afd5123bd51667e0d7a6d901c
Eluviian <bayleysapara@gmail.com>
2024-03-19 22:48:05 -0600

    why didn't this work before

--- FireProject/Assets/Scripts/Base.cs
+++ FireProject/Assets/Scripts/Base.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Base : Entity
 {
+
+    public TextMeshProUGUI baseHealth;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,10 @@ public class Base : Entity
     // Update is called once per frame
     void Update()
     {
-        
+        if (baseHealth != null)
+        {
+            baseHealth.text = "Base Health:\n" + Health.ToString() + "%";
+        }
     }
 
     public override void Death()
