85003b952d4b5e8cb56bf128e1ae206e5b79a825
Ganesh Saraswat <gsaraswa@ualberta.ca>
2024-03-24 17:28:47 -0600

    made super fancy auto commenter
    based on the wave data generate a designer friendly description

--- FireProject/Assets/Scripts/WaveDataObject.cs
+++ FireProject/Assets/Scripts/WaveDataObject.cs
@@ -6,4 +6,21 @@ public class WaveDataObject : MonoBehaviour
 {
     // using proper scriptableobjects would be prefered but for time whatever.
     public Wave wave;
+
+    private void OnValidate()
+    {
+        foreach(var c in wave.chunks)
+        {
+            string s = "";
+            if (c.waitUntilPreviousDead) s += "then ";
+            s += c.count.ToString();
+            s += "["+c.enemyType.ToString()+"] ";
+            if (c.spawnDelay > 0)
+            {
+                s += "after " + c.spawnDelay.ToString()+"s ";
+            }
+            s += "from [" + c.spawnPoint.ToString()+"]";
+            c.note = s;
+        }
+    }
 }
