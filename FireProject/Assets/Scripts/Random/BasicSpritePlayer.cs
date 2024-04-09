027240424a086eef9ba130eb7acf5aeeea271611
Ganesh Saraswat <gsaraswa@ualberta.ca>
2024-04-09 17:44:18 -0600

    stuff

--- FireProject/Assets/Scripts/Random/BasicSpritePlayer.cs
+++ FireProject/Assets/Scripts/Random/BasicSpritePlayer.cs
@@ -7,13 +7,16 @@ public class BasicSpritePlayer : MonoBehaviour
     public new SpriteRenderer renderer;
     public Sprite[] sprites;
     public float delay = 0.25f;
+    public bool faceCamera = false;
 
     int i = 0;
     float timer = 0;
+    Camera c;
 
     private void OnEnable()
     {
         renderer.sprite = sprites[i];
+        if (faceCamera) c = Camera.main;
     }
 
     private void Update()
@@ -24,6 +27,13 @@ public class BasicSpritePlayer : MonoBehaviour
             i = (i + 1) % sprites.Length;
             renderer.sprite = sprites[i];
             timer = 0;
+        }
+
+        if (faceCamera)
+        {
+            transform.LookAt(c.transform);
+            transform.Rotate(0, 180, 0);
+            transform.Rotate(0, 0, Time.timeSinceLevelLoad, Space.Self);
         }
     }
 }
