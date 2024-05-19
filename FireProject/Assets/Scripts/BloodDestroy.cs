054570f2ae66eb29af9715fe6ef8ea9bd8ee40d0
Dave Goel <davegoel29@gmail.com>
2024-05-18 22:38:00 -0600

    bloodpool bug fixed

--- FireProject/Assets/Scripts/BloodDestroy.cs
+++ FireProject/Assets/Scripts/BloodDestroy.cs
@@ -6,6 +6,9 @@ public class BloodDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject BloodDecal;
+    public LayerMask groundLayer;
+    float groundDistance;
+    Vector3 hitNormal;
 
     // Update is called once per frame
     void Awake()
@@ -16,7 +19,15 @@ public class BloodDestroy : MonoBehaviour
     IEnumerator destroy()
     {
         yield return new WaitForSeconds(0.5f);
-        Instantiate(BloodDecal, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, 10, groundLayer))
+        {
+            // Ray hit something, get the distance to the hit point
+            groundDistance = hitInfo.distance;
+            hitNormal = hitInfo.normal;
+            Debug.Log("Distance to ground: " + groundDistance);
+        }
+        GameObject inst = Instantiate(BloodDecal, new Vector3(transform.position.x, transform.position.y - groundDistance + 0.1f, transform.position.z), Quaternion.identity);
+        inst.transform.up = hitNormal;
         Destroy(gameObject);
 
     }
