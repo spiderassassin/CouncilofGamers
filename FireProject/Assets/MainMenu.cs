eca5913830e1318343e0adadfd7574bdafd7c5df
Shasta Johnsen-Sollos <s.johnsensollos@gmail.com>
2024-07-12 12:28:15 -0600

    Add new button for endless mode

--- FireProject/Assets/MainMenu.cs
+++ FireProject/Assets/MainMenu.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI highscoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         SoundManager.Instance.menu = SoundManager.Instance.CreateInstance(FMODEvents.Instance.mainMenu);
         SoundManager.Instance.menu.start();
         SoundManager.Instance.menu.release();
+
+        var highscore = PlayerPrefs.GetInt("Highscore", 0);
+        var highscoreName = PlayerPrefs.GetString("HighscoreName", "");
+        // Format with parentheses.
+        if (!string.IsNullOrEmpty(highscoreName)) highscoreName = $"({highscoreName})";
+
+        highscoreText.text = $"Highscore: {highscore}\n{highscoreName}";
     }
 
     // Update is called once per frame
