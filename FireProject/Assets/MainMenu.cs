ee1d553879e73ec6c0a0c59509c3e739c28408e5
Dave Goel <davegoel29@gmail.com>
2024-07-24 01:58:44 -0600

    added trailer to main menu

--- FireProject/Assets/MainMenu.cs
+++ FireProject/Assets/MainMenu.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 using FMODUnity;
 using FMOD.Studio;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public TextMeshProUGUI highscoreText;
     public Button endlessButton;
+    public float timer;
+    public bool trailermode = false;
+    public GameObject canvas;
+    public GameObject video;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0f;
         SoundManager.Instance.menu = SoundManager.Instance.CreateInstance(FMODEvents.Instance.mainMenu);
         SoundManager.Instance.menu.start();
         SoundManager.Instance.menu.release();
@@ -37,5 +43,43 @@ public class MainMenu : MonoBehaviour
         // Set asist mode to off when starting a new game.
         PlayerPrefs.SetInt("AssistMode", 0);
         PlayerPrefs.SetInt("EndlessMode", endless ? 1 : 0);
+    }
+
+    private void Update()
+    {
+        if (!trailermode)
+        {
+            if (!Input.anyKey)
+            {
+                timer = timer + Time.deltaTime;
+            }
+            else
+            {
+                timer = 0f;
+            }
+
+            if(timer > 19f){
+                video.SetActive(true);
+            }
+
+            if (timer > 20f)
+            {
+                timer = 0f;
+                trailermode = true;
+                //video.SetActive(true);
+                canvas.SetActive(false);
+
+            }
+        }
+
+        if (trailermode)
+        {
+            if (Input.anyKey)
+            {
+                trailermode = false;
+                video.SetActive(false);
+                canvas.SetActive(true);
+            }
+        }
     }
 }
